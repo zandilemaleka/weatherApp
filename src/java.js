e1b2d2701b452841545bd38c3f2fff127640b44c
zandilemaleka <malekabaatile@gmail.com>
2025-04-01 16:19:33 +0200

    injected more weather updates

--- src/java.js
+++ src/java.js
@@ -6,8 +6,29 @@ let cityElement = document.querySelector("#city-name");
 cityElement.innerHTML = response.data.city;
 temperatureElement.innerHTML = Math.round(temperature);
     
+let descriptionElement = document.querySelector("#weather-condition");
+descriptionElement.innerHTML = response.data.condition.description;
+let humidityElement = document.querySelector("#humidity")
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+let windElement = document.querySelector("#wind");
+windElement.innerHTML = `${response.data.wind.speed}km/h`;
+let timeElement = document.querySelector("#week-day");
+let date = new Date(response.data.time * 1000);
+timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
 
 
+let minutes = date.getMinutes();
+let hours = date.getHours();
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
